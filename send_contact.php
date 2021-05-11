e7ab8b52993000cf41d4ec50827d66e618281f19
riv02 <77107800+riv02@users.noreply.github.com>
2021-05-11 16:41:32 +0800

    Update send_contact.php

--- send_contact.php
+++ send_contact.php
@@ -1,47 +1,18 @@
 <?php
-
-$currentRequestMethod = $_SERVER['REQUEST_METHOD'];
-
-$allowedRequestMethods = array('POST', 'HEAD');
-
-if(!in_array($currentRequestMethod, $allowedRequestMethods)){
-    header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
-    exit;
-}
-
 if($_POST['kirim']){
-	$admin = 'teddyknight403@gmail.com';
+    $admin = 'teddyknight403@gmail.com';
+    
+    $name = htmlentities($_POST['name']);
+    $email = htmlentities($_POST['email']);
+    $message = htmlentities($_POST['message']);
 
-	$name = "";
-    $email = "";    
-    $message = "";
-     
-    if(isset($_POST['name'])) {
-      $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    }
-     
-    if(isset($_POST['email'])) {
-        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
-        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-    }
+    $headers = 'From: '.$email.' ' . "\r\n" .
+               'MIME-Version: 1.0' . "\r\n" .
+               'Contact-Type: text/html; charset=utf-8';
 
-	if(isset($_POST['message'])) {
-        $message = htmlspecialchars($_POST['message']);
-    }
-         
-    $headers  = 'MIME-Version: 1.0' . "\r\n"
-    .'Content-type: text/html; charset=utf-8' . "\r\n"
-    .'From: ' . $email . "\r\n";
-     
-    if(mail($admin, $name, $message, $headers)) {
-        echo "<p>Thank you for contacting us, $name. You will get a reply within 24 hours.</p>";
-		echo '<a href="index.html">Kembali ke halaman</a>';
-    } else {
-        echo '<p>We are sorry but the email did not go through.</p>';
-		echo '<a href="index.html">Kembali ke halaman</a>';
-    }
-     
-} else {
-    echo '<p>Something went wrong</p>';
+    $result = mail($admin, $name, $message, $headers);
+    var_dump($result);
+
+    echo '<a href="index.html">Kembali ke halaman</a>';
 }
 ?>
